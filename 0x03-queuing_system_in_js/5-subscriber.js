e948f6a45233a6e563cac226c3ea7b140d70e1ca
JohnIanOngayi <ongayijohnian@gmail.com>
2024-10-09 08:24:57 +0300

    task 5: completed

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+import { createClient, RedisClient } from "redis";
+
+/** @type- {} */
+const redisClient = createClient()
+  .on("error", (error) =>
+    console.log(`Redis client not connected to the server: ${error}`),
+  )
+  .on("connect", () => console.log("Redis client connected to the server"));
+
+redisClient.subscribe("holberton school channel", (error, _) => {
+  if (error) console.log(`Couldn't subscribe to the channel`);
+});
+
+redisClient.on("message", (channel, message) => {
+  if (channel === "holberton school channel") {
+    console.log(message);
+    if (message === "KILL_SERVER") {
+      redisClient.unsubscribe("holberton school channel", (error) => {
+        if (error) console.log(`Error while unsubscribing: ${error}`);
+      });
+      redisClient.quit((error) => {
+        if (error) console.log(`Error while disconnecting: ${error}`);
+      });
+    }
+  }
+});
